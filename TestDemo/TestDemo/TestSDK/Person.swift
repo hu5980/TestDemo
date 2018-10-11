d5b27b8974d61b68e326e7b09b7f4650040e6f7d
Bruce <hugy@500wan.com>
2018-10-11 17:56:09 +0800

    add pod files

---
+++ TestDemo/TestDemo/TestSDK/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  TestDemo
+//
+//  Created by Bruce on 2018/10/11.
+//  Copyright © 2018年 Bruce. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    func sayHello() {
+        print("Hello world")
+    }
+}
---
+++ TestDemo/TestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDemo
+//
+//  Created by Bruce on 2018/10/11.
+//  Copyright © 2018年 Bruce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
